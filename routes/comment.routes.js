297729bdefe18c58c7dfe320602153f43f6fcc0b
Hammad Maqsood <75095607+mhammadmaqsood@users.noreply.github.com>
2024-10-26 19:01:06 +0500

    Completed comment APIs and added indexing in mongo schema for optimized search (#7)

---
+++ routes/comment.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { addComment, getComments, deleteComment } = require("../controllers/comment.controllers");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+const router = express.Router();
+
+//ADD A COMMENT
+router.post("/add-comment", authMiddleware, addComment);
+
+//GET COMMENTS FOR A SPECIFIC POST, REEL, OR STORY
+router.get("/get-comments", authMiddleware, getComments);
+
+// Route to delete a comment
+router.delete("/delete-comment/:id", authMiddleware, deleteComment);
+
+module.exports = router;
